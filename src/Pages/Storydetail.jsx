0237aaa20c4cde642bf46dc88a2da15ba808898b
Parthpatel520 <parthpatel.13cpk@gmail.com>
2026-01-30 21:33:37 +0530

    png file change

--- src/Pages/Storydetail.jsx
+++ src/Pages/Storydetail.jsx
@@ -55,8 +55,7 @@ export default function Storydetail() {
                                 only five centuries, but also the leap into electronic typesetting,
                                 remaining essentially unchanged. It was popularised in the 1960s
                                 with the release of Letraset sheets containing Lorem Ipsum passages,
-                                and more recently with desktop publishing software like Aldus
-                                PageMaker including versions of Lorem Ipsum.
+                                and more recently with desktop publishing
                             </p>
                             <p className="font-bold">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting
